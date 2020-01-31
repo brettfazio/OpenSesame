cf18f56e9ac2f2484923cc782c3c99584f8cc3f3
Danny Parsons <rpars@knights.ucf.edu>
2020-01-31 18:59:02 -0500

    Adding my map activity page although issues with gradle or google services are making it such that it does not work

---
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/MapActivity.java
@@ -0,0 +1,18 @@
+package com.example.juleeyahwright.opensesame;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
