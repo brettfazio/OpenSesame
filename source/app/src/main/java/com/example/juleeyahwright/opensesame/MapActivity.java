17d9b8ebb0b0db965724d57ec17e317aff9b551d
Danny Parsons <rpars@knights.ucf.edu>
2020-02-01 20:12:17 -0500

    Sending along map activity despite issues

---
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/MapActivity.java
@@ -0,0 +1,34 @@
+package com.example.juleeyahwright.opensesame;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Take map to UCF to start
+        LatLng ucf = new LatLng(-28.6024, 81.2001);
+        mMap.addMarker(new MarkerOptions().position(ucf).title("Welcome to UCF!"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ucf));
+    }
+}
