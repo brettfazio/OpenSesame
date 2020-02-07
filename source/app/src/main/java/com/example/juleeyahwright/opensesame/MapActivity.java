2fe3df74ea924ac68a350bcc4d67950bdc01e7f5
Danny Parsons <rpars@knights.ucf.edu>
2020-02-06 19:55:23 -0500

    Adjusted UCF latitude and longitude, fixed camera starting point

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/MapActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/MapActivity.java
@@ -1,17 +1,22 @@
 package com.example.juleeyahwright.opensesame;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLngBounds cameraBounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +27,12 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Take map to UCF to start
-        LatLng ucf = new LatLng(-28.6024, 81.2001);
+        LatLng ucf = new LatLng(28.6024, -81.2001);
         mMap.addMarker(new MarkerOptions().position(ucf).title("Welcome to UCF!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ucf));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ucf, 16.0f));
     }
 }
