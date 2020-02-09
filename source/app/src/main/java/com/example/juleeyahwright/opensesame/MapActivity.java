ba0763f37b2cb9c88f2cf0b6ba751620a9e12f12
Jordan Carlton <jcarlton6126@gmail.com>
2020-02-09 17:36:31 -0500

    Display & maintain location information

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/MapActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/MapActivity.java
@@ -1,7 +1,7 @@
 package com.example.juleeyahwright.opensesame;
 
-import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,29 +10,49 @@ import android.view.MenuItem;
 import android.content.Intent;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.example.juleeyahwright.opensesame.CreateReport.CreateReportActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String LOCATION = "location";
+    private static final int ZOOM = 16;
+
     private GoogleMap mMap;
     private LatLng newReportLocation;
+    private boolean mLocationPermissionGranted;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location mLastKnownLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
+
+        if(savedInstanceState != null)
+        {
+            mLastKnownLocation = savedInstanceState.getParcelable(LOCATION);
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -77,6 +97,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        if(mMap != null)
+        {
+            outState.putParcelable(LOCATION, mLastKnownLocation);
+            super.onSaveInstanceState(outState);
+        }
+    }
+
     private void addReportClicked() {
         Intent intent = new Intent(MapActivity.this, CreateReportActivity.class);
         intent.putExtra("LOCATION", newReportLocation);
@@ -89,24 +119,24 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        checkAndRequestPermissions();
-
         mMap = googleMap;
-        LatLng ucf = new LatLng(28.6024, -81.2001);
-        mMap.addMarker(new MarkerOptions().position(ucf).title("Welcome to UCF!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ucf, 16.0f));
+        checkAndRequestPermissions();
+        updateMapLocation();
+        getLastKnownLocation();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                
-            } else {
-
-            }
-            return;
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            mLocationPermissionGranted = true;
         }
+        updateMapLocation();
+    }
 
     private void checkAndRequestPermissions(){
         if(ContextCompat.checkSelfPermission(
@@ -118,6 +148,61 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 MapActivity.this,
                 new String[] {ACCESS_FINE_LOCATION},
                 1);
+        } else {
+            mLocationPermissionGranted = true;
+        }
+    }
+
+    private void updateMapLocation()
+    {
+        if(mMap == null)
+            return;
+
+        try
+        {
+            if(mLocationPermissionGranted)
+            {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                getLastKnownLocation();
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                mLastKnownLocation = null;
+                checkAndRequestPermissions();
+            }
+        } catch (SecurityException e){
+        }
+    }
+
+    private void getLastKnownLocation()
+    {
+        try
+        {
+            if(mLocationPermissionGranted)
+            {
+                Task<Location> location = fusedLocationClient.getLastLocation();
+                location.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if(task.isSuccessful())
+                        {
+                            mLastKnownLocation = task.getResult();
+                            if(mLastKnownLocation != null)
+                            {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(mLastKnownLocation.getLatitude(),
+                                                mLastKnownLocation.getLongitude()),
+                                                ZOOM));
+                            }
+                        } else {
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(28.6024, -81.2001), ZOOM));
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
         }
     }
 }
