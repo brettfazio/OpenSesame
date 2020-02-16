03d667c8ba697a8468fe31c8045fd89360d0f684
Danny Parsons <rpars@knights.ucf.edu>
2020-02-15 19:13:57 -0500

    Adding updated settings stuff that's still being worked on and changed things so the API key for maps is in a secret file

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/SettingsActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/SettingsActivity.java
@@ -1,31 +1,84 @@
 package com.example.juleeyahwright.opensesame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private static final float[] NEGATIVE = {
-            -1.0f,     0,     0,    0, 255, // red
-            0, -1.0f,     0,    0, 255, // green
-            0,     0, -1.0f,    0, 255, // blue
-            0,     0,     0, 1.0f,   0  // alpha
+            -1.0f, 0, 0, 0, 255, // red
+            0, -1.0f, 0, 0, 255, // green
+            0, 0, -1.0f, 0, 255, // blue
+            0, 0, 0, 1.0f, 0  // alpha
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_activity);
+
+        // Show the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
+    public static class MainPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.key_text_size)));
+        }
+
+        private static void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+            sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(), ""));
+        }
+
+
+        private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String stringValue = newValue.toString();
+
+                if (preference instanceof ListPreference) {
+                    ListPreference listPreference = (ListPreference) preference;
+                    int index = listPreference.findIndexOfValue(stringValue);
+                    preference.setSummary(
+                            index >= 0
+                                    ? listPreference.getEntries()[index]
+                                    : null);
+
+                } else {
+                    preference.setSummary(stringValue);
+                }
+                return true;
+            }
+        };
+
+
+        @Override
+        public void onStart() {
+            super.onStart();
+        }
 
 //    protected void invertColors() {
 //        setColorFilter(new ColorMatrixColorFilter(NEGATIVE));
 //    }
 
+    }
 }
