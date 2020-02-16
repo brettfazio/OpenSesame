08dddcd9afce32c7e47331218a6cd876bbce6e8c
Danny Parsons <rpars@knights.ucf.edu>
2020-02-15 20:15:56 -0500

    removing deprecated library features because wow

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/SettingsActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/SettingsActivity.java
@@ -1,12 +1,12 @@
 package com.example.juleeyahwright.opensesame;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceFragment;
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 import android.preference.PreferenceManager;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,58 +27,12 @@ public class SettingsActivity extends AppCompatActivity {
         // Show the back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
+        
     }
 
-    public static class MainPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(final Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.settings);
 
-            bindPreferenceSummaryToValue(findPreference(getString(R.string.key_text_size)));
-        }
-
-        private static void bindPreferenceSummaryToValue(Preference preference) {
-            preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-            sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-                    PreferenceManager
-                            .getDefaultSharedPreferences(preference.getContext())
-                            .getString(preference.getKey(), ""));
-        }
-
-
-        private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                String stringValue = newValue.toString();
-
-                if (preference instanceof ListPreference) {
-                    ListPreference listPreference = (ListPreference) preference;
-                    int index = listPreference.findIndexOfValue(stringValue);
-                    preference.setSummary(
-                            index >= 0
-                                    ? listPreference.getEntries()[index]
-                                    : null);
-
-                } else {
-                    preference.setSummary(stringValue);
-                }
-                return true;
-            }
-        };
-
-
-        @Override
-        public void onStart() {
-            super.onStart();
-        }
-
-//    protected void invertColors() {
-//        setColorFilter(new ColorMatrixColorFilter(NEGATIVE));
-//    }
-
+    @Override
+    public void onStart() {
+        super.onStart();
     }
 }
