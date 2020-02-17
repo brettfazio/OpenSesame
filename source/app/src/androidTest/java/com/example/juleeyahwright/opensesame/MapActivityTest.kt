dbecb7adf42c0896211518d2c28ad7f8f53f3e6e
Danny Parsons <rpars@knights.ucf.edu>
2020-02-16 22:00:45 -0500

    removed unnecessary imports on MapActivityTest

--- source/app/src/androidTest/java/com/example/juleeyahwright/opensesame/MapActivityTest.kt
+++ source/app/src/androidTest/java/com/example/juleeyahwright/opensesame/MapActivityTest.kt
@@ -1,29 +1,19 @@
 package com.example.juleeyahwright.opensesame
 
 
-import android.content.Context
-import android.view.View
-import android.view.ViewGroup
+
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
-import androidx.test.runner.AndroidJUnit4
 import com.example.juleeyahwright.opensesame.CreateReport.CreateReportActivity
 import com.example.juleeyahwright.opensesame.Map.MapActivity
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 @LargeTest
 class MapActivityTest {
