e6f078fabcd2a2abbe4a3658873644fb3e2e7ef0
Brett Fazio <brett.fazio@hotmail.com>
2020-02-27 16:20:24 -0500

    Add back button to report detail activity

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportDetail/ReportDetailActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportDetail/ReportDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.juleeyahwright.opensesame.ReportDetail;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,20 @@ public class ReportDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_detail_activity);
+
+        // Show the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+    // End the activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Close this activity if home is selected
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
