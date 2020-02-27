6d4290bbb7ee4dc937ef28cc3e19554593a3a773
Brett Fazio <brett.fazio@hotmail.com>
2020-02-27 16:16:27 -0500

    Add report detail activity ui xml

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportDetail/ReportDetailActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportDetail/ReportDetailActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.juleeyahwright.opensesame.R;
+
 public class ReportDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO(@brettfazio): Add layout for report detail activity
+        setContentView(R.layout.report_detail_activity);
     }
 
 }
