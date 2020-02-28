9e14af4d82ee02bdaec20e6b84e2e24eb2d4789e
Brett Fazio <brett.fazio@hotmail.com>
2020-02-28 11:11:44 -0500

    naming convention update

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/Report/ReportReference.kt
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/Report/ReportReference.kt
@@ -3,9 +3,9 @@ package com.example.juleeyahwright.opensesame.Report
 import com.google.android.gms.maps.model.LatLng
 
 class ReportReference(name: String, information: String?, locationInfo: String?, location: LatLng?, documentId: String) : Report(name, information, locationInfo, location) {
-    private var documentId: String = documentId
+    private var _documentId: String = documentId
 
     fun getDocumentId(): String {
-        return documentId
+        return _documentId
     }
 }
