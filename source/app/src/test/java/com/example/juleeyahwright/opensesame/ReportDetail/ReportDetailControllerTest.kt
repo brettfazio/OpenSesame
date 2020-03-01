d2dcec2e2167c6d98d3701a8cf192c6d318486ea
Brett Fazio <brett.fazio@hotmail.com>
2020-03-01 14:26:09 -0500

    Add ReportDetailTest

---
+++ source/app/src/test/java/com/example/juleeyahwright/opensesame/ReportDetail/ReportDetailControllerTest.kt
@@ -0,0 +1,31 @@
+package com.example.juleeyahwright.opensesame.ReportDetail
+
+import com.example.juleeyahwright.opensesame.Report.ReportReference
+import com.google.android.gms.maps.model.LatLng
+import org.junit.Test
+
+class ReportDetailControllerTest {
+    var report: ReportReference = ReportReference("dummy",
+            "this is a dummy report",
+            "basement",
+            LatLng(0.0, 0.0),
+            "eFz687FFHDHD")
+
+    @Test
+    fun reportName_test() {
+        val controller = ReportDetailController(report)
+        assert(controller.reportName == "dummy")
+    }
+
+    @Test
+    fun reportInformation_test() {
+        val controller = ReportDetailController(report)
+        assert(controller.reportInformation == "this is a dummy report")
+    }
+
+    @Test
+    fun reportLocationInfo_test() {
+        val controller = ReportDetailController(report)
+        assert(controller.reportLocationInfo == "basement")
+    }
+}
