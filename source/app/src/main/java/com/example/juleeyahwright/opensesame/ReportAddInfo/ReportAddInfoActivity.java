b5e3c7edb9fb8386a7db0ed971f47b9269247bdf
Brett Fazio <brett.fazio@hotmail.com>
2020-03-05 17:27:17 -0500

    Add UI file for addInfo and setup in code

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportAddInfo/ReportAddInfoActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportAddInfo/ReportAddInfoActivity.java
@@ -1,6 +1,32 @@
 package com.example.juleeyahwright.opensesame.ReportAddInfo;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.example.juleeyahwright.opensesame.Common.BaseActivity;
+import com.example.juleeyahwright.opensesame.R;
 
 public class ReportAddInfoActivity extends BaseActivity {
+
+    // sets up the layout for the create report
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_report_activity);
+
+        // Show the back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Close this activity if home is selected
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
