4afbd546103d0d1676d1b70598d7b7d66adffc72
Brett Fazio <brett.fazio@hotmail.com>
2020-03-22 18:31:47 -0400

    Add contant URL and additional tests

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/Common/Constant.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/Common/Constant.java
@@ -5,7 +5,7 @@ import com.example.juleeyahwright.opensesame.R;
 import java.io.Serializable;
 
 public class Constant implements Serializable {
-
     public static int color = 0xfff68f21;
     public static int appTheme = R.style.AppTheme_amber;
+    public static String UCF_WORK_ORDER_URL = "https://fo.ucf.edu/CRForm";
 }
