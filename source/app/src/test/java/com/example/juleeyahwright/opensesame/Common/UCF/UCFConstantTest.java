ba7526bc7da6859c60102ac51a3948d2effa2c80
Brett Fazio <brett.fazio@hotmail.com>
2020-03-22 19:13:07 -0400

    Add additional UCF test

--- source/app/src/test/java/com/example/juleeyahwright/opensesame/Common/UCF/UCFConstantTest.java
+++ source/app/src/test/java/com/example/juleeyahwright/opensesame/Common/UCF/UCFConstantTest.java
@@ -13,7 +13,16 @@ public class UCFConstantTest {
     @Test
     public void inUCF_test() {
         boolean res = UCFConstant.isContained(new LatLng(28.6025, -81.2017));
+        assert (res);
+    }
 
+    /*
+    Testing: Points outside of UCF return false
+    Pass Criteria: createResult returns false
+    */
+    @Test
+    public void notInUCF_test() {
+        boolean res = UCFConstant.isContained(new LatLng(8.6025, 0.2017));
         assert (res);
     }
 
