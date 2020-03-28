d7f27657c06eb8cfdb4d37e44f3fcb55a92b6d8c
Danny Parsons <rpars@knights.ucf.edu>
2020-03-28 12:33:16 -0400

    Adding reportEditInfoActivity changes

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportEditInfo/ReportEditInfoActivity.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportEditInfo/ReportEditInfoActivity.java
@@ -2,6 +2,7 @@ package com.example.juleeyahwright.opensesame.ReportEditInfo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ import com.example.juleeyahwright.opensesame.Report.ReportReference;
 import com.example.juleeyahwright.opensesame.ReportDetail.ReportDetailController;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
@@ -28,8 +30,14 @@ import java.util.Objects;
 public class ReportEditInfoActivity extends BaseActivity {
 
     private ReportDetailController controller;
-
     private RecyclerView.LayoutManager layoutManager;
+    Report reportToEdit = null;
+    String intentUid = null;
+    String intentTitle = null;
+    String intentDesc = null;
+    String reportUid = null;
+    String reportTitle = null;
+    String reportDesc = null;
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,28 +49,37 @@ public class ReportEditInfoActivity extends BaseActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         Intent intent = getIntent();
-        final String uid = intent.getStringExtra("reportUID");
-        String title = intent.getStringExtra("reportTitle");
-        String desc = intent.getStringExtra("reportDescription");
+        intentUid = intent.getStringExtra("reportUID");
+        intentTitle = intent.getStringExtra("reportTitle");
+        intentDesc = intent.getStringExtra("reportDescription");
 
         Query data = FirebaseDatabase.getInstance().getReference().child("reports").orderByChild("uid").equalTo("reportUID");
 
-        FirebaseDatabase.getInstance().getReference().child("reports")
-            .addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                        if(uid != null && snapshot.getValue("uid")) {
-//
-//                        }
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        reportToEdit = snapshot.getValue(Report.class);
+                        reportUid = reportToEdit.getUID();
+                        reportTitle = reportToEdit.getName();
+                        reportDesc = reportToEdit.getInformation();
                     }
                 }
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                }
-            });
+            }
+            @Override
+            public void onCancelled(DatabaseError dbError) {
 
-        setFields();
+            }
+        };
+
+        data.addListenerForSingleValueEvent(valueEventListener);
+
+        Log.d("tags", reportToEdit.getName());
+
+        if(reportToEdit != null) {
+            setFields();
+        }
 
         Button addButton = findViewById(R.id.reportDetailEditButton);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -79,40 +96,21 @@ public class ReportEditInfoActivity extends BaseActivity {
     private void setFields() {
         setTitleField();
         setInfoField();
-        setLocationInfoField();
+        // setLocationInfoField();
     }
 
     private void setTitleField() {
-        TextView textView = findViewById(R.id.reportDetailHeader);
-        textView.setVisibility(View.GONE);
-        EditText editText = null;
-        String currentInfo = null;
-        if(controller != null) {
-            if(controller.getReportName() != null) {
-                textView.setText(controller.getReportName());
-                currentInfo = (String) textView.getText();
-        }
-        }
-        editText = findViewById(R.id.editTitleHeader);
-        if(currentInfo != null)
-            editText.setText(currentInfo);
+        EditText editText = findViewById(R.id.editTitleHeader);
+        editText.setText(reportToEdit.getName());
         editText.setVisibility(View.VISIBLE);
     }
 
     private void setInfoField() {
-        TextView textView = findViewById(R.id.reportDetailInfo);
-        textView.setVisibility(View.GONE);
-        EditText editText = null;
-        String currentInfo = null;
-        if(controller != null) {
-            if(controller.getReportInformation() != null) {
-                textView.setText(controller.getReportInformation());
-                currentInfo = (String) textView.getText();
-            }
-        }
-        editText = findViewById(R.id.editReportDetailInfo);
-        if(currentInfo != null)
-            editText.setText(currentInfo);
+        EditText editText = findViewById(R.id.editReportDetailInfo);
+        // editText.setText(reportToEdit.getInformation());
+        Log.d("tags", "info here: ");
+        Log.d("tags", "uid: " + reportUid + "\ntitle: " + reportTitle + "\ndesc: " + reportDesc);
+        editText.setText("HEREJKLDJKLJLDSKJLJFKLDSJFKDFJSDL F");
         editText.setVisibility(View.VISIBLE);
     }
 
