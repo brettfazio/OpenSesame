3cd413ec1c15f5c203764bb3a0b1620be7cc43e5
Jordan Carlton <jcarlton6126@gmail.com>
2020-03-29 09:51:29 -0400

    Added null handling for all report fields

--- source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportList/ReportListSorter.java
+++ source/app/src/main/java/com/example/juleeyahwright/opensesame/ReportList/ReportListSorter.java
@@ -16,12 +16,36 @@ public class ReportListSorter implements Comparator<ReportListItem> {
 
     public int compare(ReportListItem rli1, ReportListItem rli2) {
         if (compareType == 0) {
+            if (rli1.getReportName() == null) {
+                return (rli2.getReportName() == null) ? 0 : 1;
+            }
+            if (rli2.getReportName() == null) {
+                return 1;
+            }
             return rli1.getReportName().compareTo(rli2.getReportName());
         } else if (compareType == 1) {
+            if (rli1.getReportLocation() == null) {
+                return (rli2.getReportLocation() == null) ? 0 : 1;
+            }
+            if (rli2.getReportLocation() == null) {
+                return 1;
+            }
             return rli1.getReportLocation().compareTo(rli2.getReportLocation());
         } else if (compareType == 2){
+            if (rli1.getReportDescription() == null) {
+                return (rli2.getReportDescription() == null) ? 0 : 1;
+            }
+            if (rli2.getReportDescription() == null) {
+                return 1;
+            }
             return rli1.getReportDescription().compareTo(rli2.getReportDescription());
         } else {
+            if (rli1.getReportDistance() == null) {
+                return (rli2.getReportDistance() == null) ? 0 : 1;
+            }
+            if (rli2.getReportDistance() == null) {
+                return 1;
+            }
             return (rli1.getReportDistance().compareTo(rli2.getReportDistance()));
         }
     }
